5b260157b750a9db691b656719fe361e9b4e37bf
Fred <fred.mny@gmail.com>
2022-12-04 18:19:03 -0300

    feat(sparkify_etl): configure staging tasks

--- airflow/dags/sparkify_etl.py
+++ airflow/dags/sparkify_etl.py
@@ -29,12 +29,22 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag,
-    
+    table='staging_events',
+    redshift_conn_id='redshift_connection',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity_dend',
+    s3_key='log_data/{execution_date.year}/{execution_date.month}'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table='staging_songs',
+    redshift_conn_id='redshift_connection',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity_dend',
+    s3_key='song_data',
+    json_format='s3://udacity-dend/log_json_path.json'
 )
 
 load_songplays_table = LoadFactOperator(
